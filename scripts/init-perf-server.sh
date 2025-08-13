1600d9a9d1050933d08ebc9888cb73bbc998ace9
Brian Denton <bdenton@paloaltonetworks.com>
2025-08-13 07:09:37 -0700

    Cleanup VM init script

--- scripts/init-perf-server.sh
+++ scripts/init-perf-server.sh
@@ -1,33 +1,32 @@
 #! /usr/bin/env bash
 
+SSL_KEY_FILE=/etc/ssl/private/nginx-selfsigned.key
+SSL_CRT_FILE=/etc/ssl/certs/nginx-selfsigned.crt
+SSL_SUBJECT="/C=US/ST=WA/L=Redmond/O=Azure Cloud Partnership/CN=azure.csp.paloaltonetworks.com"
 apt update -y
 apt upgrade -y
 apt install nginx curl iperf wrk -y
-openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/ssl/private/nginx-selfsigned.key -out /etc/ssl/certs/nginx-selfsigned.crt -subj "/C=US/ST=WA/L=Redmond/O=Azure Cloud Partnership/CN=azure.csp.paloaltonetworks.com"
-cat <<'EOF' > /etc/nginx/sites-available/default-ssl
+openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout ${SSL_KEY_FILE} -out ${SSL_CRT_FILE} -subj ${SSL_SUBJECT}
+cat <<EOF > /etc/nginx/sites-available/default-ssl
 server {
         listen 443 ssl default_server;
         listen [::]:443 ssl default_server;
 
-        ssl_certificate /etc/ssl/certs/nginx-selfsigned.crt;
-        ssl_certificate_key /etc/ssl/private/nginx-selfsigned.key;
+        ssl_certificate ${SSL_CRT_FILE};
+        ssl_certificate_key ${SSL_KEY_FILE};
 
-        # Include a strong cipher suite to improve security
         ssl_protocols TLSv1.2 TLSv1.3;
         ssl_prefer_server_ciphers on;
         ssl_ciphers EECDH+AESGCM:EDH+AESGCM;
 
         root /var/www/html;
 
-        # Add index.php to the list if you are using PHP
         index index.html index.htm index.nginx-debian.html;
 
         server_name _;
 
         location / {
-                # First attempt to serve request as file, then
-                # as directory, then fall back to displaying a 404.
-                try_files $uri $uri/ =404;
+                try_files \$uri \$uri/ =404;
         }
 }
 EOF
